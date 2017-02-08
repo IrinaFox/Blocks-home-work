1b759a2d730ecb53e70eb23df26f0a97da36612a
Liss <irina.fox88@gmail.com>
2017-02-08 04:24:04 +0200

    Made closure

--- scripts/Button.js
+++ scripts/Button.js
@@ -13,4 +13,38 @@ function createWindow () {
                      '<div class="contentDiv"> </div>';
 
     content.innerHTML += contentDiv;
+
+    createButtonMine('RED', 'blockRed', content);
+
+
 }
+
+
+function createButtonMine (value, className, changedDiv) {
+    var counter = makeCounter();
+    var content = document.getElementById('content');
+    var button = document.createElement('input');
+    button.type = 'text';
+    button.value = value;
+
+
+
+    button.addEventListener('click', function () {
+        changedDiv.removeAttribute('class');
+        changedDiv.removeAttribute(className);
+
+        var count = counter();
+        console.log(count);
+    });
+    content.appendChild(button);
+}
+
+function makeCounter() {
+    var currentCount = 1;
+
+    return function() { // (**)
+        return currentCount++;
+    };
+}
+
+
