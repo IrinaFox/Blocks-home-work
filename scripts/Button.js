6ba62a2b41a278305f54812d925ccb0d38ce98c4
Liss <irina.fox88@gmail.com>
2017-02-09 03:31:26 +0200

    Less code

--- scripts/Button.js
+++ scripts/Button.js
@@ -1,60 +1,3 @@
-//add events to buttons
-function addEvents2 () {
-    var content = document.getElementById('content'),
-        buttonRed = (document.getElementsByName('red'))[0],
-        buttonBlue = (document.getElementsByName('blue'))[0],
-        buttonGreen = (document.getElementsByName('green'))[0],
-        block = (document.getElementsByClassName('block'))[0],
-        counterRed = makeCounter(),
-        counterBlue = makeCounter(),
-        counterGreen = makeCounter(),
-        changedRed =':counterRed',
-        changedBlue =':counterBlue',
-        changedGreen =':counterGreen';
-
-
-    buttonRed.addEventListener('click', function () {
-        var countRed = counterRed();
-
-        block.removeAttribute('class');
-        block.setAttribute('class', 'blockRed');
-
-        changeCounter(countRed, 'Red');
-
-    }, false);
-
-
-
-    buttonBlue.addEventListener('click', function () {
-
-        var countBlue = counterBlue();
-
-        block.removeAttribute('class');
-        block.setAttribute('class', 'blockBlue');
-
-        changeCounter(countBlue, 'Blue');
-
-    }, false);
-
-
-    buttonGreen.addEventListener('click', function () {
-        console.log('WORK');
-        var countGreen= counterGreen();
-
-        block.removeAttribute('class');
-        block.setAttribute('class', 'blockGreen');
-
-        changeCounter(countGreen, 'Green');
-
-    }, false);
-
-
-}
-
-
-
-
-
 //function-closure for counter
 function makeCounter() {
     var currentCount = 1;
@@ -67,10 +10,50 @@ function makeCounter() {
 
 //Make alive counter
 function changeCounter (newElement, counterColor) {
-    var counter = (document.getElementsByClassName('counter'+counterColor))[0];
+    var counter = (document.getElementsByClassName('counter'+counterColor))[0],
+        changedCount = counter.innerHTMl = counterColor + ': ' + newElement;
 
-    var changedCount = counter.innerHTMl = counterColor + ': ' + newElement;
     counter.innerHTML = changedCount;
+}
+
+//add events to buttons
+function addEvents () {
+    var buttonRed = (document.getElementsByName('red'))[0],
+        buttonBlue = (document.getElementsByName('blue'))[0],
+        buttonGreen = (document.getElementsByName('green'))[0],
+        counterRed = makeCounter(),
+        counterBlue = makeCounter(),
+        counterGreen = makeCounter();
+
+    buttonRed.addEventListener('click', function () {
+        var block = document.getElementById('block'),
+            counter = counterRed();
+
+        block.removeAttribute('class');
+        block.setAttribute('class', 'blockRed');
+
+        changeCounter(counter, 'Red');
+    }, false);
+
+    buttonBlue.addEventListener('click', function () {
+        var block = document.getElementById('block'),
+            counter = counterBlue();
+
+        block.removeAttribute('class');
+        block.setAttribute('class', 'blockBlue');
+
+        changeCounter(counter, 'Blue');
+    }, false);
+
+    buttonGreen.addEventListener('click', function () {
+        var block = document.getElementById('block'),
+            counter = counterGreen();
+
+        block.removeAttribute('class');
+        block.setAttribute('class', 'blockGreen');
+
+        changeCounter(counter, 'Green');
+    }, false);
 }
 
 
@@ -96,26 +79,25 @@ function changeCounter (newElement, counterColor) {
 
 
 
+var ira = function (button, Color, callback) {
+    var countGreen = callback();
+    var block = document.getElementById('block');
+    block.removeAttribute('class');
+    block.setAttribute('class', 'blockGreen');
 
+    changeCounter(countGreen, 'Green');
 
-
-
+};
 
 
 //add events to buttons
-function addEvents () {
-    var content = document.getElementById('content'),
-        buttonRed = (document.getElementsByName('red'))[0],
+function addEvents3 () {
+    var buttonRed = (document.getElementsByName('red'))[0],
         buttonBlue = (document.getElementsByName('blue'))[0],
         buttonGreen = (document.getElementsByName('green'))[0],
-
         counterRed = makeCounter(),
         counterBlue = makeCounter(),
-        counterGreen = makeCounter(),
-        changedRed =':counterRed',
-        changedBlue =':counterBlue',
-        changedGreen =':counterGreen';
-
+        counterGreen = makeCounter();
 
     buttonRed.addEventListener('click', function () {
         var countRed = counterRed();
@@ -141,16 +123,10 @@ function addEvents () {
     }, false);
 
 
-    buttonGreen.addEventListener('click', function () {
-        console.log('WORK');
-        var countGreen= counterGreen();
-        var block = document.getElementById('block');
-        block.removeAttribute('class');
-        block.setAttribute('class', 'blockGreen');
-
-        changeCounter(countGreen, 'Green');
-
+    buttonGreen.addEventListener('click', function()  {
+        ira(buttonGreen, 'Green', counterGreen);
     }, false);
 
-
 }
+
+
