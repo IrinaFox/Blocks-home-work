cf0a6c78b2284cc2ff5c5d2e1c5e1982130e6f61
Liss <irina.fox88@gmail.com>
2017-02-09 04:26:35 +0200

    Works and beautiful

--- scripts/DomTpl.js
+++ scripts/DomTpl.js
@@ -1,7 +1,10 @@
 var buttonTpl = '<div class="mainDiv buttonContainer">\
                          <input type="button" name="red" value="RED" class="button buttonRed"/>\
+                         <br><br>\
                          <input type="button" name="blue" value="BLUE" class="button buttonBlue"/>\
+                         <br><br>\
                          <input type="button" name="green" value="GREEN" class="button buttonGreen"/>\
+                         <br><br>\
                      </div>\
                      <div class="mainDiv">\
                          <div id="block">\
